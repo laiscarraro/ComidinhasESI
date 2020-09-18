ced3cadb88fabc659f62fb1c44003f374f93fb7f
Matheus Kurianski <matheuskurianski@usp.br>
2020-09-17 21:11:04 -0300

    Corrige formatação de preço e adiciona mais testes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
   validates :name, presence: { message: "É obrigatório informar o nome do produto" }
   validates :price, presence: { message: "É obrigatório informar o preço do produto" },
-    format: { with: /\A\d+(?:\.\d{2})?\z/ }, numericality: { greater_than: 0, less_than: 10000 }
+   format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 10000 }
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,19 +4,29 @@ RSpec.describe Product, type: :model do
   it 'invalido sem nome' do
     product = Product.new
     product.price = 10.99
+    product.description = "salve"
     expect(product).not_to be_valid
   end
 
   it 'invalido sem preço' do
     product = Product.new
     product.name = "Cone"
+    product.description = "salve"
     expect(product).not_to be_valid
+  end
+
+  it 'valido sem descrição' do
+    product = Product.new
+    product.name = "Cone"
+    product.price = 19.5
+    expect(product).to be_valid
   end
 
   it 'preço string e não número' do
     product = Product.new
     product.name= "Cone"
     product.price = "a,@gmail.com"
+    product.description = "salve"
     expect(product).not_to be_valid
   end
 
@@ -24,6 +34,23 @@ RSpec.describe Product, type: :model do
     product = Product.new
     product.name= "Cone"
     product.price = 312.213
+    product.description = "salve"
     expect(product).not_to be_valid
+  end
+
+  it 'preço com uma casa decimal' do
+    product = Product.new
+    product.name= "Cone"
+    product.price = 10.5
+    product.description = "salve"
+    expect(product).to be_valid
+  end
+
+  it 'preço com nenhuma casa decimal' do
+    product = Product.new
+    product.name= "Cone"
+    product.price = 15
+    product.description = "salve"
+    expect(product).to be_valid
   end
 end
