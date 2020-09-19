0085e3b7b19dea7b407bf71cab45a70a38fa755c
José Luiz <borinjr@joseluiz-PC.localdomain>
2020-09-19 20:52:59 -0300

    Esqueleto dos testes para home

---
+++ features/step_definitions/home.rb
@@ -0,0 +1,67 @@
+Dado('que estou na página home') do
+    visit 'home/index'
+  end
+  
+  Então('deve aparecer para mim todas as comidinhas') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando('eu preencho o campo {string} com {string}') do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando('aperto {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então('devo ver comidinhas que contenham esse nome') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando('aperto em uma categoria') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então('deve mostrar apenas comidinhas da categoria selecionada') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando('aperto no icone de explorar') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então('vou para a página explorar') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando('aperto no icone de perfil') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então('deve mostrar as informações do usuário') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando('estou sem internet') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então('deve mostrar mensagem {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando('pesquiso uma comidinha que não existe') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então('devo ver uma mensagem de {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando('escolho uma comidinha') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então('vou para a página dessa comidinha') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
