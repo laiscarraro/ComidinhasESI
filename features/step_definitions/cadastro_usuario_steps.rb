e41e9252234026c77d005c531d97c8f5512a2b54
Lais Carraro Leme Cavalheiro <lcavalheiro@uolinc.com>
2020-09-18 21:31:22 -0300

    criar testes com cucumber

---
+++ features/step_definitions/cadastro_usuario_steps.rb
@@ -0,0 +1,39 @@
+Dado('que estou na paǵina de cadastro de usuário') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando('preencho o campo {string} com {string}') do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando('clico em Concordo com todos os Termos e Condições') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando('clico em Criar') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então('ele deve ter sido salvo no banco de dados') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então('ser redirecionado para a home') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Dado('que estou na página de cadastro de usuário') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando('deixo o campo {string} vazio') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então('deverei ver a mensagem de erro {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando('não clico em Aceitar os termos e condições') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
