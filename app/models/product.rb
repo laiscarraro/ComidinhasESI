12293417b8d4413757e226c55f583db8746f366b
Matheus Kurianski <matheuskurianski@usp.br>
2020-09-20 16:17:09 -0300

    Enforça preço entre R$ 0,50 e R% 10,000 e adiciona mensagem de erro

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
   validates :name, presence: { message: "É obrigatório informar o nome do produto" }
   validates :price, presence: { message: "É obrigatório informar o preço do produto" },
-   format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 10000 }
+   format: { with: /\A\d+(?:\.\d{0,2})?\z/, message: "O formato de preço é inválido" }, numericality: { greater_than: 0.49, less_than: 10000, message: "O preço precisa ser um número entre R$ 0,50 e R$ 10,000"}
 end
