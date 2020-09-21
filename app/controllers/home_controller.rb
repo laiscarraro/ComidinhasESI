0b2f31a9a22ac0d8873dc52ad33b49ceaca19392
José Luiz <borinjr@joseluiz-PC.localdomain>
2020-09-21 17:22:52 -0300

    Controller e routes atualizados

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
     def index
+        @products = Product.all
+    end
+
+    def show
+        @products = Product.find_by(name: params[:name])
+        render 'index'
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
     root 'home#index'
+    get 'home/index'
+    get 'home/:name' => 'home#show'
     resources :product, :except => [:index]
-
 end
