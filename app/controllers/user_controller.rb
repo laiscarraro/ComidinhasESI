8dc1803cbf71465b843be82bd472e2f8d6d43c42
Lais Carraro Leme Cavalheiro <lcavalheiro@uolinc.com>
2020-09-24 19:32:32 -0300

    merge com viewerpoint

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,7 +7,6 @@ class UserController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            #redirect_to 'home'
         else
             render 'new'
         end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2020_09_21_002236) do
 
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "username"
