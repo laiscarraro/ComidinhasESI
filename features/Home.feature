6d8eb5ea0bb5f8e47d16467e665530a8aadd0823
José Luiz <borinjr@joseluiz-PC.localdomain>
2020-09-26 00:45:47 -0300

    Remoção dos testes de funcionalidades que serão implementadas em iterações futuras

--- features/Home.feature
+++ features/Home.feature
@@ -15,33 +15,8 @@ Quando eu preencho o campo "prodname" com "Con"
 E aperto em pesquisar
 Então devo ver comidinhas que contenham esse nome como "Cones do Wilson"
 E não devo ver comidinhas que contenham o nome "Lanches Naturais" ou "Cereais"
- 
-Cenário: Acessar comidinhas em uma categoria
-Dado que estou na página home
-Quando aperto em uma categoria
-Então deve mostrar apenas comidinhas da categoria selecionada 
-
-Cenário: Explorar produtos
-Dado que estou na página home
-Quando aperto no icone de explorar
-Então vou para a página explorar
-
-Cenário: Acessar dados do usuário
-Dado que estou na página home
-Quando aperto no icone de perfil
-Então deve mostrar as informações do usuário
-
-Cenário: Sem internet
-Dado que estou na página home
-Quando estou sem internet
-Então deve mostrar mensagem "Conecte-se a internet para ultilizar o comidinhas"
 
 Cenário: Não encontrou a comidinha
 Dado que estou na página home
 Quando pesquiso uma comidinha que não existe
 Então devo ver uma mensagem de "sem resultados"
-
-Cenário: Acessar comidinhas
-Dado que estou na página home
-Quando escolho uma comidinha
-Então vou para a página dessa comidinha
--- features/step_definitions/home.rb
+++ features/step_definitions/home.rb
@@ -44,38 +44,6 @@ Dado('que estou na página home') do
     expect(page).to have_no_content(string2)
   end
   
-  Quando('aperto em uma categoria') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Então('deve mostrar apenas comidinhas da categoria selecionada') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Quando('aperto no icone de explorar') do
-    click_link 'explore'
-  end
-  
-  Então('vou para a página explorar') do
-    expect(page).to have_current_path('/explore/index')
-  end
-  
-  Quando('aperto no icone de perfil') do
-    click_link 'user'
-  end
-  
-  Então('deve mostrar as informações do usuário') do
-    expect(page).to have_current_path('/user/index')
-  end
-  
-  Quando('estou sem internet') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Então('deve mostrar mensagem {string}') do |string|
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
   Quando('pesquiso uma comidinha que não existe') do
     fill_in 'search', :with => 'ruby on rails is aweasome'
     click_on 'inspect'
@@ -83,12 +51,4 @@ Dado('que estou na página home') do
   
   Então('devo ver uma mensagem de {string}') do |string|
     expect(page).to have_content(string)
-  end
-  
-  Quando('escolho uma comidinha') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Então('vou para a página dessa comidinha') do
-    pending # Write code here that turns the phrase above into concrete actions
   end
