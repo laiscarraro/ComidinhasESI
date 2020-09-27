1340ec74a863173de8b5b70c1a64dc98ac43075a
karacurt <38267570+karacurt@users.noreply.github.com>
2020-09-27 02:10:39 -0300

    Revert "Merge branch 'master' into home"
    This reverts commit c587db0515b3f7c19e8e449de63ab841cc1e5fd3, reversing
    changes made to b903fecabbf40ee970c5f820f71694a0a47f2687.

--- db/migrate/20200922211638_categories.rb
+++
@@ -1,8 +0,0 @@
-class Categories < ActiveRecord::Migration[6.0]
-  def change
-    create_join_table :categories, :products do |t|
-      t.index :category_id
-      t.index :product_id
-    end
-  end
-end
