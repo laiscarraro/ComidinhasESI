737c83d9ec3c6a30265acbc36022f088c3b241e0
José Luiz <borinjr@joseluiz-PC.localdomain>
2020-10-06 01:19:28 -0300

    Design da tela de login

--- app/javascript/channels/login.js
+++ app/javascript/channels/login.js
@@ -7,19 +7,26 @@ window.addEventListener("load", function() {
         user.addEventListener("focus", addClassLoginUserHighlight);
         user.addEventListener("blur", removeClassLoginUserHighlight);
     }
+
+    var password = document.getElementById("login-password-text")
+
+    if(password) {
+        password.addEventListener("focus", addClassLoginPasswordHighlight);
+        password.addEventListener("blur", removeClassLoginPasswordHighlight);
+    }
 });
 
 function addClassLoginUserHighlight() {
-    document.getElementById("login-user").classList.add("login-user-border");
-    document.getElementById("login-user-label").classList.add("login-user-label-float");
+    document.getElementById("login-user").classList.add("login-border");
+    document.getElementById("login-user-label").classList.add("login-label-float");
     document.getElementById("user-check").classList.remove("user-check-draw");
 }
 
 function removeClassLoginUserHighlight() {
-    document.getElementById("login-user").classList.remove("login-user-border");
+    document.getElementById("login-user").classList.remove("login-border");
 
     if(document.getElementById("login-user-text").value == "") {
-        document.getElementById("login-user-label").classList.remove("login-user-label-float");
+        document.getElementById("login-user-label").classList.remove("login-label-float");
     }
     else {
         checkUserExists("/login/user_exists").then(function() {
@@ -42,4 +49,17 @@ function checkUserExists(url) {
         };
         xhr.send();
     });
+}
+
+function addClassLoginPasswordHighlight() {
+    document.getElementById("login-password").classList.add("login-border");
+    document.getElementById("login-password-label").classList.add("login-label-float");
+}
+
+function removeClassLoginPasswordHighlight() {
+    document.getElementById("login-password").classList.remove("login-border");
+
+    if(document.getElementById("login-password-text").value == "") {
+        document.getElementById("login-password-label").classList.remove("login-label-float");
+    }
 }
