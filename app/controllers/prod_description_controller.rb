3f836e950a3bbadbc4194c04cf6bfc5173e4e9da
unknown <karacurt@github.com>
2020-10-07 03:43:55 -0300

    prod-img-desc-ratings

--- app/controllers/prod_description_controller.rb
+++ app/controllers/prod_description_controller.rb
@@ -1,2 +1,14 @@
 class ProdDescriptionController < ApplicationController
+    def description
+        @categories = Category.all
+        @products = Product.all
+        render 'description'
+    end
+
+    def category
+        @categories = Category.all
+        /category = Category.find(params[:id])*/   
+        render 'description'
+    end
+
 end
