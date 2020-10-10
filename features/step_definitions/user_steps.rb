19fbf5733b5429703006c144716a7d1f11114f2b
amandaventurin <amandacventurin@gmail.com>
2020-10-10 16:46:10 -0300

    fixing steps

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -10,18 +10,10 @@ Dado('que estou na página do usuário') do
     visit 'new'
   end
   
-  Quando('clico em produto') do
-    #click_on 'product'
-  end
-  
-  Então('o usuario deve ser redirecionado para a página do produto') do
-    #visit 'product'
-  end
-  
   Quando('clico sair') do
     click_on 'logout'
   end
   
   Então('o usuário deve ser redirecionado para a página de login') do
-    #visit 'login'
+    visit 'login'
   end
--- features/user.feature
+++ features/user.feature
@@ -11,11 +11,6 @@ Dado que estou na página do usuário
 Quando clico em adicionar produto
 Então o usuário deve ser redirecionado para a página de adicionar produto 
 
-Cenário: Visualizar meus produtos
-Dado que estou na página do usuário
-Quando clico em produto
-Então o usuario deve ser redirecionado para a página do produto
-
 Cenário: Logout
 Dado que estou na página do usuário
 Quando clico sair
