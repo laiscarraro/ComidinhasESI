9904454746983b6538d3ab222de0321b7a170353
Daniel Lima <daniel-lima@usp.br>
2020-10-09 23:14:00 -0300

    unused-route-removed

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
     get 'home/index'
     get 'home/search' => 'home#search'   
     get 'home/category' => 'home#category'
-    get 'product/description' => 'product#description'
     resources :product, :except => [:index]
 end
