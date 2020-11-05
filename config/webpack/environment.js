dcdda937094f9b6a5ead7e8872654f9b63ce157a
LeviUSP <levifernandes@usp.br>
2020-11-05 18:04:34 +0100

    desconfigure webpack

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,15 +1,12 @@
-const { environment, config } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker')
 const webpack = require ('webpack')
-
-const WebpackerPwa = require('webpacker-pwa')
-new WebpackerPwa(config, environment)
-
-// environment.plugins.append('Provide',
-// new webpack.ProvidePlugin({
-//     $:'jquery',
-//     jQuery: 'jquery',
-//     Popper: ['popper.js','default']
-// })
-// )
+environment.plugins.append('Provide',
+new webpack.ProvidePlugin({
+    $:'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js','default']
+})
+)
 
 module.exports = environment
+
