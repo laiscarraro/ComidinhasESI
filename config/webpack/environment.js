fbac264ec20802ea89c5965d764b5d112b51c8c0
LeviUSP <levifernandes@usp.br>
2020-11-05 17:34:11 +0100

    configura webpack e adiciona icones para utilização offline

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,15 @@
-const { environment } = require('@rails/webpacker')
+const { environment, config } = require('@rails/webpacker')
 const webpack = require ('webpack')
-environment.plugins.append('Provide',
-new webpack.ProvidePlugin({
-    $:'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js','default']
-})
-)
+
+const WebpackerPwa = require('webpacker-pwa')
+new WebpackerPwa(config, environment)
+
+// environment.plugins.append('Provide',
+// new webpack.ProvidePlugin({
+//     $:'jquery',
+//     jQuery: 'jquery',
+//     Popper: ['popper.js','default']
+// })
+// )
+
 module.exports = environment
