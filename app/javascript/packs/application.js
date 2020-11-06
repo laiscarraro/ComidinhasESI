bcf093be758ee2c5374c5d702681f97170cddbbd
LeviUSP <levifernandes@usp.br>
2020-11-06 10:59:23 +0100

    service-worker added to public directory (guide: https://onrails.blog/2019/01/08/easy-pwas-the-rails-way/)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,13 @@ require("channels")
 
 import 'bootstrap'
 
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/service-worker.js', { scope: './' })
+    .then(function(reg) {
+      console.log('[Companion]', 'Service worker registered!');
+      console.log(reg);
+    });
+}
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
