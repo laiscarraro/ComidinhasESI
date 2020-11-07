7f51c05fdad8f8fdb554d9b575b73a026abbe117
Matheus Kurianski <matheuskurianski@usp.br>
2020-11-07 17:40:28 -0300

    Previne a inserção de duplicatas

--- app/controllers/suggestion_vote_controller.rb
+++ app/controllers/suggestion_vote_controller.rb
@@ -6,7 +6,7 @@ class SuggestionVoteController < ApplicationController
     suggestion_vote = SuggestionVote.new
     suggestion_vote.user = current_user
     suggestion_vote.suggestion = @suggestion
-    @suggestion.suggestion_votes << suggestion_vote
+    @suggestion.suggestion_votes << suggestion_vote unless @suggestion.suggestion_votes.exists?({user_id: current_user.id})
     redirect_to suggestions_path
   end
 
