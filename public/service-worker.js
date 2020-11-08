f2905f565827a20150ac0fe7584969193efd6a53
LeviUSP <levifernandes@usp.br>
2020-11-08 15:06:15 +0100

    refatoração do código

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,31 +6,23 @@ const OFFLINE_URL = 'offline';
 self.addEventListener('install', function (event) {
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
-    // Setting {cache: 'reload'} in the new request will ensure that the response
-    // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
     await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
   })());
 });
 
 self.addEventListener('activate', async function (event) {
   event.waitUntil((async () => {
-  // Enable navigation preload if it's supported.
-  // See https://developers.google.com/web/updates/2017/02/navigation-preload
     if ('navigationPreload' in self.registration) {
       await self.registration.navigationPreload.enable();
     }
   })());
 
-  // Tell the active service worker to take control of the page immediately.
   self.clients.claim();
 });
 
 self.addEventListener('fetch', function (event) {
-  // We only want to call event.respondWith() if this is a navigation request
-  // for an HTML page.
     event.respondWith((async () => {
       try {
-        // First, try to use the navigation preload response if it's supported.
         const preloadResponse = await event.preloadResponse;
         if (preloadResponse) {
           return preloadResponse;
@@ -38,11 +30,8 @@ self.addEventListener('fetch', function (event) {
 
         return await caches.match(event.request) || await fetch(event.request);
       } catch (error) {
-        // catch is only triggered if an exception is thrown, which is likely
-        // due to a network error.
-        // If fetch() returns a valid HTTP response with a response code in
-        // the 4xx or 5xx range, the catch() will NOT be called.
-        console.log('Fetch failed; returning offline page instead.', error);
+        // catch utilizado para página offline (fetch não funcionou)
+        console.error('Fetch failed; returning offline page instead.', error);
 
         const cache = await caches.open(CACHE_NAME);
         const cachedResponse = await cache.match(OFFLINE_URL);
