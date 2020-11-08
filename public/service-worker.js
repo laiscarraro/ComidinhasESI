c9e97ba14cef53c7537875824414276420459992
LeviUSP <levifernandes@usp.br>
2020-11-08 15:02:28 +0100

    listeners do service-worker refatorados e página offline estilizada

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,60 +1,51 @@
-var CACHE_VERSION = 'v1';
-var CACHE_NAME = CACHE_VERSION + ':sw-cache-';
+const OFFLINE_VERSION = 1;
+const CACHE_NAME = 'offline';
+const OFFLINE_URL = 'offline';
 
-function onInstall(event) {
-  console.log('[Serviceworker]', "Rails Service Worker Installing!", event);
-  event.waitUntil((async() => {
+self.addEventListener('install', function (event) {
+  event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
+    // Setting {cache: 'reload'} in the new request will ensure that the response
+    // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
+    await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
+  })());
+});
 
-    await cache.addAll([
-      // make sure serviceworker.js is not required by application.js
-      // if you want to reference application.js from here
-      '<%%#= asset_path "application.js" %>',
-      '<%%= asset_path "application.css" %>',
-    ]);
-  }))
-}
+self.addEventListener('activate', async function (event) {
+  event.waitUntil((async () => {
+  // Enable navigation preload if it's supported.
+  // See https://developers.google.com/web/updates/2017/02/navigation-preload
+    if ('navigationPreload' in self.registration) {
+      await self.registration.navigationPreload.enable();
+    }
+  })());
 
-function onActivate(event) {
-  console.log('[Serviceworker]', "Rails Service Worker Activating!", event);
+  // Tell the active service worker to take control of the page immediately.
+  self.clients.claim();
+});
 
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          // Return true if you want to remove this cache,
-          // but remember that caches are shared across
-          // the whole origin
-          return cacheName.indexOf(CACHE_VERSION) !== 0;
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
-    })
-  );
-}
-
-// Borrowed from https://github.com/TalAter/UpUp
-function onFetch(event) {
-  event.respondWith(
-    // try to return untouched request from network first
-    fetch(event.request).catch(function() {
-      // if it fails, try to return request from the cache
-      return caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
+self.addEventListener('fetch', function (event) {
+  // We only want to call event.respondWith() if this is a navigation request
+  // for an HTML page.
+    event.respondWith((async () => {
+      try {
+        // First, try to use the navigation preload response if it's supported.
+        const preloadResponse = await event.preloadResponse;
+        if (preloadResponse) {
+          return preloadResponse;
         }
-        // if not found in cache, return default offline content for navigate requests
-        if (event.request.mode === 'navigate' ||
-          (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
-          console.log('[Serviceworker]', "Rails Service Worker Fetching offline content", event);
-          return caches.match('/offline.html');
-        }
-      })
-    })
-  );
-}
 
-self.addEventListener('install', onInstall);
-self.addEventListener('activate', onActivate);
-self.addEventListener('fetch', onFetch);  
+        return await caches.match(event.request) || await fetch(event.request);
+      } catch (error) {
+        // catch is only triggered if an exception is thrown, which is likely
+        // due to a network error.
+        // If fetch() returns a valid HTTP response with a response code in
+        // the 4xx or 5xx range, the catch() will NOT be called.
+        console.log('Fetch failed; returning offline page instead.', error);
+
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResponse = await cache.match(OFFLINE_URL);
+        return cachedResponse;
+      }
+    })());
+});
