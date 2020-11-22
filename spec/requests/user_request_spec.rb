96ff923794f1b82142c643b777283bad84f95396
Matheus Kurianski <matheuskurianski@usp.br>
2020-11-22 19:10:53 -0300

    Rspec
    Co-authored-by: amandaventurin <amandacventurin@gmail.com>

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  it "deve mostrar métodos de pagamento do usuário do usuario" do 
+    avatar = fixture_file_upload(File.join(Rails.root, 'public', 'estudante.jpg'))
+    User.create(username: "Reinaldo do Dogão", email: "reinaldoDoDogao@gmail.com", password: "Rei123", avatar: avatar, vr: true, card: true)
 
+    get '/user/1'
+    expect(response).to be_successful
+    expect(response.body).to include('<p>VR</p>')
+    expect(response.body).to include('<p>Cartão</p>')
+  end
 end
