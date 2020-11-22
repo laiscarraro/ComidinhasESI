02a43b9996103c630110c602845c237d0242b82f
Laís Cavalheiro <laiscarraro@usp.br>
2020-11-22 16:41:44 -0300

    Adicionando testes unitários

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,47 +6,69 @@ RSpec.describe User, type: :model do
     @avatar = fixture_file_upload(File.join(Rails.root, 'public', 'estudante.jpg'))
   end
 
-  it 'usuario valido' do
-    user = User.new
-    user.username = "Reinaldo do Dogão"
-    user.email = "reinaldoDoDogao@gmail.com"
-    user.password = "Rei123"
-    user.avatar = @avatar
-    expect(user).to be_valid
+  context 'usuario valido' do
+    it 'deveria considerar o usuario como valido' do
+      user = User.new
+      user.username = "Reinaldo do Dogão"
+      user.email = "reinaldoDoDogao@gmail.com"
+      user.password = "Rei123"
+      user.avatar = @avatar
+      expect(user).to be_valid
+    end
   end
 
-  it 'invalido sem dados' do
-    user = User.new
-    expect(user).not_to be_valid
+  context 'invalido sem dados' do
+    it 'deveria considerar o usuario como invalido' do
+      user = User.new
+      expect(user).not_to be_valid
+    end
   end
 
-  it 'invalido sem email' do
-    user = User.new
-    user.username = "teste"
-    user.password = "senha"
-    expect(user).not_to be_valid
+  context 'invalido sem email' do
+    it 'deveria considerar o usuario como invalido' do
+      user = User.new
+      user.username = "teste"
+      user.password = "senha"
+      expect(user).not_to be_valid
+    end
   end
 
-  it 'invalido sem username' do
-    user = User.new
-    user.email = "teste@gmail.com"
-    user.password = "senha"
-    expect(user).not_to be_valid
+  context 'invalido sem username' do
+    it 'deveria considerar o usuario como invalido' do
+      user = User.new
+      user.email = "teste@gmail.com"
+      user.password = "senha"
+      expect(user).not_to be_valid
+    end
   end
 
-  it 'invalido sem senha' do
-    user = User.new
-    user.email = "teste@gmail.com"
-    user.username = "teste"
-    expect(user).not_to be_valid
+  context 'invalido sem senha' do
+    it 'deveria considerar o usuario como invalido' do
+      user = User.new
+      user.email = "teste@gmail.com"
+      user.username = "teste"
+      expect(user).not_to be_valid
+    end
   end
 
-  it 'valido sem avatar' do
-    user = User.new
-    user.username = "Reinaldo do Dogão"
-    user.email = "reinaldoDoDogao@gmail.com"
-    user.password = "Rei123"
-    expect(user).to be_valid
+  context 'valido sem avatar' do
+    it 'deveria considerar o usuario como valido' do
+      user = User.new
+      user.username = "Reinaldo do Dogão"
+      user.email = "reinaldoDoDogao@gmail.com"
+      user.password = "Rei123"
+      expect(user).to be_valid
+    end
   end
 
+  context 'sem produtos' do
+    it 'deveria retornar a contagem de produtos como zero' do
+      user = User.new
+      user.username = "Reinaldo do Dogão"
+      user.email = "reinaldoDoDogao@gmail.com"
+      user.password = "Rei123"
+      user.id = 1
+      expect(Product.joins(:user).where("user_id = #{user.id}").count).to eq(0)
+    end
+  end
 end
